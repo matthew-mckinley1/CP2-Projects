debfea84235512cfb4e892df09f1ac0a85288de8
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-01-10 10:36:29 -0700

    i need to get freq and goal working AIOAJOIEJd

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -10,33 +10,43 @@ Tip Calculator
 
 def goal():
     totalGoal = int(input("What would you like the goal to be? :"))
-    howMuch = int(input("How much money would you like to deposit every week? :"))
+    weekorMonth = str(input("Would you like to deposit Weekly or Monthly? (W/M)")).upper()
+    if weekorMonth == "W":
+        WoM = "week"
+    elif weekorMonth == "M":
+        WoM = "month"
+
+    
+
+    howMuch = int(input("How much money would you like to deposit every week/month? :"))
     howLong = totalGoal/howMuch
-    print("It will take", howLong, "weeks to reach your goal!")
+    print("It will take", howLong, "weeks to reach your goal! \n \n \n")
+    main()
 
 
 def compIntCalc():
-    start = int(input("How much money are you starting with?"))
-    rate = int(input("What is the rate of interest? (Percentage Anually)"))
-    compound = int(input("How many times will it compound anually?"))
+    start = float(input("How much money are you starting with?"))
+    rate = float(input("What is the rate of interest? (Percentage Anually)"))
+    compound = float(input("How many times will it compound anually?"))
     years  = int(input("How many years are you going to leave it?"))
-    rate2 = int(rate/100)
+    rate = float(rate/100)
     #broke it down further, wasn't working with the other commented formula
-    rateCompound = 1 + (rate2 / compound)
-    print(rateCompound)
+    rateCompound = 1 + (rate / compound)
     compTime = compound * years
     rateCompCompYear = rateCompound**compTime
     total2 = start * rateCompCompYear
     #total = start(1 + (rate/compound))**(compound * years)
-    total2 = int(total2)
-    print(total2)
+    total2 = float(total2)
+    print("You will end up with", total2, "dollars! \n \n \n")
+    main()
     
 def budgetAlloc():
-    budget = int(input("What is your monthly income?"))
-    needs = (budget*.5)
-    wants = (budget*.3)
-    saving = (budget*.2)
-    print("You should put", needs, "toward things you need,", wants, "toward things you want, and", saving, "toward saving up for something, out of your total", budget)
+    budget = float(input("What is your monthly income?"))
+    needs = float(budget*.5)
+    wants = float(budget*.3)
+    saving = float(budget*.2)
+    print("You should put", needs, "dollars toward things you need,", wants, "dollars toward things you want, and", saving, "dollars toward saving up for something, out of your total", budget, "budget! \n \n \n")
+    main()
 
 
 
@@ -44,7 +54,8 @@ def salePriceCalc():
     price = int(input("What is the price of the item you are buying? :"))
     discount = int(input("What is the percentage discount of that item? :"))
     newPrice = price * (1 - (discount/100))
-    print(newPrice)
+    print("With this discount, you only have to pay", newPrice, "dollars! \n \n \n")
+    main()
 
 
 
@@ -52,7 +63,8 @@ def tipCalc():
     price = int(input("What is the price of the item that you are finding the tip for? :"))
     tip = int(input("How much are you going to tip (percent) :"))
     totalPrice = price * (1 + (tip/100))
-    print(totalPrice)
+    print("You will have to pay", totalPrice, "dollars total! \n \n \n")
+    main()
 
 
 def main():
