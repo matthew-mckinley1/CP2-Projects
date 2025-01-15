602da7cee1eea3bcd3f3d0759fe95e47ab2bde7f
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-01-15 10:43:58 -0700

    JKWAG UIWAH WAAHUA HUIL FHUWAHkwda

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -1,72 +1,63 @@
 #Matthew McKinley Financial Calculator
-"""
-How long it will take to save for a goal based on a weekly or monthly deposit
-Compound Interest Calculator 
-Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
-Sale Price Calculator (apply discounts to prices)
-Tip Calculator
-"""
 
-
+#Calculates how long it will take to save for a goal
 def goal():
-    totalGoal = int(input("What would you like the goal to be? :"))
+    totalGoal = float(input("What would you like the goal to be? :"))
     weekorMonth = str(input("Would you like to deposit Weekly or Monthly? (W/M)")).upper()
     if weekorMonth == "W":
         WoM = "week"
     elif weekorMonth == "M":
         WoM = "month"
 
-    
-
-    howMuch = int(input("How much money would you like to deposit every week/month? :"))
+    howMuch = float(input("How much money would you like to deposit every week/month? :"))
     howLong = totalGoal/howMuch
-    print("It will take", howLong, "weeks to reach your goal! \n \n \n")
-    main()
+    print(f"It will take {howLong:.2f} weeks to reach your goal! \n \n \n")
+    runAgain()
 
-
+#Calculates compound interest
 def compIntCalc():
     start = float(input("How much money are you starting with?"))
     rate = float(input("What is the rate of interest? (Percentage Anually)"))
     compound = float(input("How many times will it compound anually?"))
-    years  = int(input("How many years are you going to leave it?"))
+    years  = float(input("How many years are you going to leave it?"))
     rate = float(rate/100)
-    #broke it down further, wasn't working with the other commented formula
+#Calculating based off of the inputs
     rateCompound = 1 + (rate / compound)
     compTime = compound * years
     rateCompCompYear = rateCompound**compTime
     total2 = start * rateCompCompYear
-    #total = start(1 + (rate/compound))**(compound * years)
     total2 = float(total2)
-    print("You will end up with", total2, "dollars! \n \n \n")
-    main()
+    print(f"You will end up with {total2:.2f} dollars! \n \n \n")
+    runAgain()
     
+#Allocates a budget based off of the income
 def budgetAlloc():
     budget = float(input("What is your monthly income?"))
     needs = float(budget*.5)
     wants = float(budget*.3)
     saving = float(budget*.2)
-    print("You should put", needs, "dollars toward things you need,", wants, "dollars toward things you want, and", saving, "dollars toward saving up for something, out of your total", budget, "budget! \n \n \n")
-    main()
+    print(f"You should put {needs:.2f} dollars toward things you need, {wants:.2f} dollars toward things you want, and {saving:.2f} dollars toward saving up for something, out of your total {budget:.2f} budget! \n \n \n")
+    runAgain()
 
 
-
+#Finds sale price of something after a discount
 def salePriceCalc():
-    price = int(input("What is the price of the item you are buying? :"))
-    discount = int(input("What is the percentage discount of that item? :"))
+    price = float(input("What is the price of the item you are buying? :"))
+    discount = float(input("What is the percentage discount of that item? :"))
     newPrice = price * (1 - (discount/100))
-    print("With this discount, you only have to pay", newPrice, "dollars! \n \n \n")
-    main()
+    print(f"With this discount, you only have to pay {newPrice:.2f} dollars! \n \n \n")
+    runAgain()
 
-
-
+#Finds the tip based off of the price and the percent you are going to tip
 def tipCalc():
-    price = int(input("What is the price of the item that you are finding the tip for? :"))
-    tip = int(input("How much are you going to tip (percent) :"))
-    totalPrice = price * (1 + (tip/100))
-    print("You will have to pay", totalPrice, "dollars total! \n \n \n")
-    main()
+    price = float(input("What is the price of the item that you are finding the tip for? :"))
+    tip = float(input("How much are you going to tip (percent) :"))
+    tip2 = float(1 + (tip/100))
+    totalPrice = price * tip2
+    print(f"You will have to pay {totalPrice:.2f} dollars total! \n \n \n")
+    runAgain()
 
-
+#defining the main function
 def main():
     choice = int(input(" Press 1 to see how long it will take to save for a goal \n Press 2 for a Compound Interest Calculator \n Press 3 for a Budget Allocator \n Press 4 for a Sale Price Calculator \n Press 5 for a Tip Calculator \n :"))
     if choice == 1:
@@ -83,4 +74,16 @@ def main():
         print("You did not input one of the options! Try again.")
         main()
 
+
+def runAgain():
+    again = str(input("Would you like to run it again? (Y/N)"))
+    if again == "Y":
+        main()
+    elif again == "N":
+        exit()
+    else:
+        print("You didn't put a Y or an N \n")
+        runAgain()
+
+#calling the main function so that it actually runs
 main()
