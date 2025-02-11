a3cac0022b20f5e28782e1df79754e657f0546b4
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-11 04:08:18 +0000

    airplane sounds

--- Personal Library Program/main.py
+++ Personal Library Program/main.py
@@ -3,13 +3,58 @@
 books = []
 titles = []
 
+#Print simple and complicated list thingy
+def printy():
+    choose = int(input("Would you like a simple or a detailed list of the information (1 for simple, 2 for detailed)"))
+    if choose == 1:
+        for i in books:
+            print(f"{i["Title"]} by {i["Author"]}")
+    elif choose == 2:
+        for i in books:
+            print(f"{i["Title"]} by {i["Author"]} with {i["Pages"]} pages and it is a {i["Genre"]} book")
+    else:
+        print("You didn't put a 1 or a 2, try again.")
+        printy()
+
+
 #add the book function
 def add_book(books, titles):
-    title = str(input("What is the title of the book you want to add?\n"))
-    author = str(input("What is the author of that book?\n"))
-    all = (title, author)
+    title = input("What is the title of the book you want to add?\n")
+    author = input("What is the author of that book?\n")
+    pages = int(input("How many pages long is the book?\n"))
+    genre = input("What genre is the book?\n")
+    all = {
+        "Title":title,
+        "Author":author,
+        "Pages":pages,
+        "Genre":genre
+    }
     titles.append(title)
     books.append(all)
+
+
+def edit_book():
+    #make new things to change the previous variables
+    takeOut = str(input("What book would you like to edit?"))
+    if takeOut in titles:
+        indx = titles.index(takeOut)
+        del books[indx]
+        del books[titles]
+        title = input("What is the new title of the book?\n")
+        author = input("What is the new author of that book?\n")
+        pages = int(input("How many pages long is the new book?\n"))
+        genre = input("What genre is the new book?\n")
+        all = {
+            "Title":title,
+            "Author":author,
+            "Pages":pages,
+            "Genre":genre
+        }
+        titles.append(title)
+        books.append(all)
+    elif takeOut not in titles:
+        return("This is not in the list!")
+
 
 #search function
 def search_func(books, titles):
@@ -33,16 +78,18 @@ def remove_func(books, titles):
 #main function to always run until they exit, having choices of what they want to do
 def main(books, titles):
     while True:
-        choice = int(input("\n\nPress 1 to print the list\nPress 2 to add to the list\nPress 3 to search through the list\nPress 4 to remove something from the list\nPress 5 to exit the program\n:"))
+        choice = int(input("\n\nPress 1 to print the list\nPress 2 to add to the list\nPress 3 to edit a book\nPress 4 to search through the list\nPress 5 to remove something from the list\nPress 6 to exit the program\n:"))
         if choice == 1:
-            print(books)
+            printy()
         elif choice == 2:
             add_book(books, titles)
         elif choice == 3:
-            search_func(books, titles)
+            edit_book()
         elif choice == 4:
-            remove_func(books, titles)
+            search_func(books, titles)
         elif choice == 5:
+            remove_func(books, titles)
+        elif choice == 6:
             exit()
         else:
             print("You didn't put in a usable number!")
