e40be2e7beac0ea3cff192e35ddc354d475bf024
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-19 10:50:36 -0700

    no time

---
+++ To Do List/main.py
@@ -0,0 +1,10 @@
+#Matthew McKinley To Do List
+
+#Create a program that allows the user to view, add, delete, and mark tasks on a to do list that is saved on a seperate text file.
+#Create a to do list (Kept on a txt file)
+#Add items to the to do list
+#Mark item as complete
+#Delete item from to do list
+
+def add_items():
+    with open("To Do List\to_do_list.txt", "a"):
