a3c1fe408351202527dcf7544689ba002b55a44a
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-21 10:24:26 -0700

    switchy slashes

--- To Do List/main.py
+++ To Do List/main.py
@@ -17,28 +17,28 @@ def view_items():
 
 def add_items():
     with open("To Do List\to_do_list.txt", "a") as file:
-        add = input("What would you like to add to the to do list?")
+        add = input("What item would you like to add?")
         #"To Do List\to_do_list.txt".append(add) this is not how it works
         main()
 
 
 def mark_items():
     with open("To Do List\to_do_list.txt", "w+") as file:
-        mark = input("What would you like to mark as done in the to do list")
+        mark = input("What item would you like to mark as done?")
         #add something to show that they're marked
         main()
 
 
 def delete_items():
     with open("To Do List\to_do_list.txt", "w") as file:
-        delete = input("What would you like to delete from the to do list?")
+        delete = input("What item would you like to delete?")
         #remove stuff
         main()
 
 
 def main():
     while True:
-        choice = input("Welcome to the To Do List!\nPress 1 to View To Do List\nPress 2 to Add Items\nPress 3 to Mark Items as Done\nPress 4 to Delete Items\nPress 5 to Leave\n:")
+        choice = input("Welcome to the To Do List!\nPress 1 to View\nPress 2 to Add Items\nPress 3 to Mark Items\nPress 4 to Delete Items\nPress 5 to Leave\n:")  
         if choice == 1:
             view_items()
         elif choice == 2:
