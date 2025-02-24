2c96c91005ded42818e6a23868f134d3f184eee7
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-02-24 10:17:34 -0700

    *keyboard typing sounds*

---
+++ Notes/multi_page/calcs.py
@@ -0,0 +1,13 @@
+#Matthew McKinley, basic math equations
+
+def add(num_one, num_two):
+    return (num_one+num_two)
+
+def sub(num_one, num_two):
+    return (num_one-num_two)
+
+def div(num_one, num_two):
+    return (num_one/num_two)
+
+def mult(num_one, num_two):
+    return (num_one*num_two)
---
+++ Notes/multi_page/main.py
@@ -0,0 +1,4 @@
+#Matthew McKinley, 
+from calcs import add as x
+
+print(x(4,6))
