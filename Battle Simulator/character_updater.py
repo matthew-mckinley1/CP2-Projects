9856c2995ac0b41f8a31e0cd0dd833f461d7232f
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-03-06 10:51:50 -0700

    case 1 pass and now it doesnt throw an error at me but it also doesnt do anything tragic

---
+++ Battle Simulator/character_updater.py
@@ -0,0 +1,8 @@
+#Character Updater
+
+def new_char():
+    name = input("What is your character's name?")
+    health = input("What is your character's health?") #change these to be 1-100, maybe make it so they only have 300 or 200 points able to be used
+    strength = input("What is your character's strength?")
+    defense = input("What is your character's defense?")
+    speed = input("What is your character's speed?")
---
+++ Battle Simulator/main.py
@@ -0,0 +1,17 @@
+#Matthew McKinley Battle Simulator
+
+def main():
+    while True:
+        match input("Press 1 to create a new character\nPress 2 to see a character's stats\nPress 3 to edit a character\nPress 4 to battle two characters\nPress 5 to exit the game\n:"):
+            case "1":
+                pass #new_char()
+            case "2":
+                pass #char_stats()
+            case "3":
+                pass #char_edit()
+            case "4":
+                pass #battle()
+            case "5":
+                pass #break
+            case _:
+                print("That isn't an applicable number!!")
